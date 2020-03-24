25fa5b33d5c790c1055734947a6b207d44f3c50c
jrandall <jessica.morgan.randall@emory.edu>
2020-03-24 09:18:02 -0400

    updated file struc. completed clean task

---
+++ clean/src/clean.R
@@ -0,0 +1,47 @@
+
+#!/usr/bin/env Rscript --vanilla
+# set expandtab ft=R ts=4 sw=4 ai fileencoding=utf-8
+#
+# Author: JR
+# Maintainer(s): JR
+# License: ?
+#
+# -----------------------------------------------------------
+# COVID19/clean/src/clean.R
+
+pacman::p_load("tidyverse", "lubridate", "knitr", "here", "assertr", 
+               "incidence", "janitor", "forcats", "scales")
+
+files <- list(input_data= here::here("COVID19/clean/input/full_data.csv"),
+              cleaned_data = here::here("COVID19/write/input/full_data_clean.csv"), 
+              count_data = here::here("COVID19/write/input/full_data_clean.csv"))
+
+stopifnot(length(files) == 2)
+
+# clean up full data set
+# shorten location to loc
+# change reserved word date to date_rec to indicate date recorded
+# keep all counts of cases and deaths in new dataframe
+# populate missing dates and add in counts of 0 
+#   (ex: in Afghanistan, no dates between from 03 March 20 -- 08 March 2020 were recorded)
+#   and are now listed as having 0 cases or deaths on these dates
+
+cases_032320 <- as.data.frame(readr::read_delim(files$input_data, delim = ",")) %>%
+  clean_names() %>%
+  transmute(loc = as.factor(location),
+         date_rec = as.Date(date, "%Y/%m/%d"), 
+         new_cases = as.double(new_cases), 
+         new_deaths = as.double(new_deaths), 
+         total_cases = as.double(total_cases), 
+         total_deaths = as.double(total_deaths)) %>%
+  group_by(loc) %>% 
+  complete(loc, date_rec = seq(min(date_rec), max(date_rec), by = 'day')) %>%
+  replace(., is.na(.), 0)
+
+cases_032320  <- cases_032320 %>%
+  verify(ncol(cases_032320) == 6 & (nrow(cases_032320) == 1266656)) %>%
+  verify(is.factor(loc) & is.Date(date_rec)) %>%
+  verify(is.na(new_cases) == FALSE)%>% 
+  write_delim(files$cleaned_data, delim = "|")
+
+###done###
